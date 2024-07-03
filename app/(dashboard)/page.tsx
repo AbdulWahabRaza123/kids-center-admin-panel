bd0a41334cf2af982744634877c829d0b2c14ab6
AbdulWahabRaza123 <abdulwahabraza123@gmail.com>
2024-07-04 00:18:45 +0500

    first page has been completed

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,3 +1,105 @@
+"use client";
+import { SelectInput } from "@/components/ui/inputs/select-input";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+
+const options = [
+  {
+    title: "Student",
+    value: "student",
+  },
+];
+const tableHeadings = [
+  "ID",
+  "Name",
+  "Class",
+  "Age",
+  "Gender",
+  "Teacher",
+  "Email",
+];
+const data = [
+  {
+    id: 1,
+    profile: "/assets/profile.svg",
+    name: "Raza",
+    class: "English",
+    age: 20,
+    gender: "Male",
+    teacherName: "David",
+    email: "raza@gmail.com",
+  },
+  {
+    id: 2,
+    profile: "/assets/profile.svg",
+    name: "Raza",
+    class: "English",
+    age: 20,
+    gender: "Male",
+    teacherName: "David",
+    email: "raza@gmail.com",
+  },
+  {
+    id: 3,
+    profile: "/assets/profile.svg",
+    name: "Raza",
+    class: "English",
+    age: 20,
+    gender: "Male",
+    teacherName: "David",
+    email: "raza@gmail.com",
+  },
+];
 export default function Home() {
-  return <main>Hello</main>;
+  const [mount, setMount] = useState<boolean>(false);
+  const [value, setValue] = useState<string>("");
+  useEffect(() => {
+    setMount(true);
+  }, []);
+  if (!mount) return null;
+  return (
+    <main className="flex flex-col px-10">
+      <div className="flex flex-row items-center justify-between">
+        <h1 className="text-[26px] font-[600]">Welcome!</h1>
+        <SelectInput options={options} value={value} setValue={setValue} />
+      </div>
+      <table className="w-full mt-10">
+        <thead className="bg-[#7A1FA01A]">
+          {tableHeadings.map((heading) => (
+            <th key={heading} className="w-[200px] text-start p-3">
+              {heading}
+            </th>
+          ))}
+        </thead>
+        <tbody>
+          {data.map((val) => {
+            return (
+              <>
+                <tr>
+                  <td className="w-[200px] text-start p-3">{val.id}</td>
+                  <td className="flex flex-row items-center gap-2 w-[200px] text-start p-3">
+                    <Image
+                      src={val.profile}
+                      width={30}
+                      height={30}
+                      alt="profile"
+                      className="rounded-full object-cover"
+                    />
+                    <p>{val.name}</p>
+                  </td>
+                  <td className="w-[200px] text-start p-3">{val.class}</td>
+                  <td className="w-[200px] text-start p-3">{val.age}</td>
+                  <td className="w-[200px] text-start p-3">{val.gender}</td>
+                  <td className="w-[200px] text-start p-3">
+                    {val.teacherName}
+                  </td>
+                  <td className="w-[200px] text-start p-3">{val.email}</td>
+                </tr>
+              </>
+            );
+          })}
+        </tbody>
+      </table>
+    </main>
+  );
 }
---
+++ components/ui/inputs/select-input.tsx
@@ -0,0 +1,35 @@
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+export const SelectInput = ({
+  options,
+  value,
+  setValue,
+}: {
+  options: {
+    title: string;
+    value: string;
+  }[];
+  value: string;
+  setValue: (val: string) => void;
+}) => {
+  return (
+    <>
+      <Select onValueChange={(value) => setValue(value)} value={value}>
+        <SelectTrigger className="w-[350px] rounded-[10px]">
+          <SelectValue placeholder="Select a value" />
+        </SelectTrigger>
+        <SelectContent>
+          {options.map((val) => {
+            return <SelectItem value={val.value}>{val.title}</SelectItem>;
+          })}
+        </SelectContent>
+      </Select>
+    </>
+  );
+};
